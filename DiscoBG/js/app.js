64d47938cb2c254b7e96db099910548a02f4e5a3
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-03-13 11:13:10 +0530

    made small changes

--- DiscoBG/js/app.js
+++ DiscoBG/js/app.js
@@ -4,14 +4,18 @@ function colorChanger() {
   for (let index = 0; index < 6; index++) {
     colorRandom += hex[Math.floor(Math.random() * 16)];
   }
-  console.log(colorRandom);
+  //   console.log(colorRandom);
   return colorRandom;
 }
 
 document.getElementById("color-change").addEventListener("click", () => {
   const color = colorChanger();
   document.body.style.backgroundColor = color;
+  document.querySelector(".box").style.borderColor = color;
   document.getElementById("color-code").innerHTML = color;
   document.getElementById("color-code").style.color = color;
   document.getElementById("color-change").style.color = color;
+  navigator.clipboard.writeText(color);
 });
+
+colorChanger();
