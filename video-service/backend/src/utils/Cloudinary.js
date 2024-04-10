3533f48744e656ba20df4c03aeb17bde96cd4ea4
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-04-10 19:23:11 +0530

    setup for uploading file using multer and cloudinary

---
+++ video-service/backend/src/utils/Cloudinary.js
@@ -0,0 +1,33 @@
+import cloudinary from "cloudinary";
+import fs from "fs"; // file system for file handling inheriently given with nodejs
+
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file is uploaded successfully
+    console.log(
+      "file uploaded successfully on cloudinary " +
+        response.format +
+        " at " +
+        response.url
+    );
+    return response;
+  } catch (error) {
+    // remove the locally saved temp file as failed to uplod the file
+    fs.unlinkSync(localFilePath);
+  }
+};
